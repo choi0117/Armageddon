0d9bbb26ab94c8eb559b0317f83bc625c2564f5f
csbin0117 <csbin0117@gmail.com>
2025-04-11 16:40:53 +0900

    [feat]MonsterUI
    몬스터 체력바를 추가했습니다.

--- Armageddon/Assets/Scripts/Monster.cs
+++ Armageddon/Assets/Scripts/Monster.cs
@@ -1,30 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+// 몬스터 스크립트
 public class Monster : MonoBehaviour
 {
-    public int maxHp = 20;
-    private int currentHp;
+    // 몬스터의 체력
+    public int maxHp = 100;
+    // 몬스터의 체력
+    public int currentHp;
 
-    private void Start()
+    public GameObject healthBarPrefab;
+    private Image fillImage;
+    private GameObject healthBarInstance;
+
+    void Start()
     {
+        // 체력 초기화
         currentHp = maxHp;
+
+        // 체력바 생성
+        if (healthBarPrefab != null)
+        {
+            // 체력바 프리팹을 인스턴스화
+            healthBarInstance = Instantiate(healthBarPrefab, transform.position + Vector3.up * 1f, Quaternion.identity);
+            // 체력바를 캔버스의 자식으로 설정
+            healthBarInstance.transform.SetParent(GameObject.Find("Canvas").transform, false);
+            // 체력바의 위치를 몬스터의 위치에 맞게 조정
+            fillImage = healthBarInstance.transform.Find("Fill").GetComponent<Image>();
+        }
+    }
+
+    void Update()
+    {
+        // 체력바의 위치를 몬스터의 위치에 맞게 조정
+        if (healthBarInstance != null)
+        {
+            // 체력바의 위치를 몬스터의 위치에 맞게 조정
+            healthBarInstance.transform.position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 0.2f);
+        }
     }
 
     public void TakeDamage(int damage)
     {
+        // 데미지를 받아 체력을 감소시킴
         currentHp -= damage;
-        Debug.Log($"{gameObject.name} took {damage} damage. HP: {currentHp}");
 
         if (currentHp <= 0)
         {
             Die();
         }
+
+        UpdateHealthBar();
     }
 
-    private void Die()
+    void UpdateHealthBar()
     {
+        if (fillImage != null)
+        {
+            fillImage.fillAmount = (float)currentHp / maxHp;
+        }
+    }
+
+    void Die()
+    {
+        Destroy(healthBarInstance);
         Destroy(gameObject);
     }
 }
